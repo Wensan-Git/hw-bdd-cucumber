f121370d50c752a4416bdf3ebeffefb954642569
Jiacheng Yin <jy3280@columbia.edu>
2022-10-11 17:35:17 +0000

    part 2 complete

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,10 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-  end
-  pending "Fill in this step in movie_steps.rb"
+    Movie.create(movie) end
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -14,6 +14,7 @@ end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
+
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
@@ -27,20 +28,42 @@ end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
+  actual_list = rating_list.split(",")
+  if uncheck == nil
+    for item in actual_list
+      check(item)
+    end
+  else
+    for item in actual_list
+      uncheck(item)
+    end
+  end
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 # Part 2, Step 3
 Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  actual_list = movie_list.split(",")
+  if no
+    for item in actual_list
+      not expect(page).to have_content(text)
+    end
+  else
+    for item in actual_list
+      expect(page).to have_content(text)
+    end
+  end
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  rows = ((page.all('table#movies td').count)/4).to_i
+  expect(rows).to eq 10
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 ### Utility Steps Just for this assignment.
